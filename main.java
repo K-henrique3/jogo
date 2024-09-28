e73c1fee05f641d1c576eb29cac6bc7d4dfd1e0d
K-henrique3 <khauanrique556@gmail.com>
2024-09-27 22:49:12 -0300

    Update Main.java

--- main.java
+++ main.java
@@ -1,8 +1,8 @@
-package adivinhe;
+
 import java.util.Random;
 import java.util.Scanner;
 
-public class main {
+public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Random rd = new Random();
